849d01b912da41ec8c6c4d084cfbfe04b443dedf
Gokul N K <nkgokul@gmail.com>
2015-02-06 01:17:19 +0530

    Changes to .gitignore file to add client www folder

---
+++ client/www/js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('UsersCtrl', function($scope, Users) {
+  Users.all().then(function(users){
+    $scope.users = users;
+  });
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('SettingsCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
